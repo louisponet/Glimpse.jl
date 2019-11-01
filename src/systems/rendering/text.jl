646d1b25862a5e9a0c97419bd54aeed94699cc16
Unknown <louisponet@gmail.com>
2019-11-02 00:44:09 +0100

    fixed fontrendering to be done by a distance field

--- src/systems/rendering/text.jl
+++ src/systems/rendering/text.jl
@@ -17,22 +17,21 @@ function Overseer.update(::TextUploader, m::AbstractLedger)
 	text = m[Text]
 	vao  = m[TextVao]
 	prog = m[TextProgram][1]
+	ucolor = m[UniformColor]
+	spatial= m[Spatial]
 	fontstorage = m[FontStorage][1]
 
-	for e in @entities_in(text)
+	for e in @entities_in(text && spatial && ucolor && !vao)
     	t=text[e]
-		space_o_wh, uv_offset_width  = to_gl_text(t, fontstorage)
-		vao[e] = TextVao(VertexArray([generate_buffers(prog.program,
-		                                                        GEOMETRY_DIVISOR,
-		                                                        uv=Vec2f0.([(0, 1),
-		                                                                    (0, 0),
-		                                                                    (1, 1),
-		                                                                    (1,0)]));
-                                               generate_buffers(prog.program,
-                                                                GLint(1),
-                                                                space_o_wh      = space_o_wh,
-                                                                uv_offset_width = uv_offset_width)],
-                                               collect(0:3), GL_TRIANGLE_STRIP, length(t.str)), true)
+		offset_width, uv_texture_bbox  = to_gl_text(t, fontstorage)
+		nsprites = length(t.str)
+		vao[e] = TextVao(VertexArray(generate_buffers(prog.program,
+                                                      GLint(0),
+                                                      color = fill(ucolor[e].color, nsprites),
+                                                      rotation = fill(Vec4f0(0), nsprites),
+                                                      offset_width    = offset_width,
+                                                      uv_texture_bbox = uv_texture_bbox),
+                                      GL_POINTS), true)
 	end
 end
 
@@ -49,7 +48,7 @@ function to_gl_text(string::AbstractString, textsize::Int, font::Vector{Ptr{AP.F
     uv_offset_width = AP.glyph_uv_width!.(Ref(atlas), chars, (font,))
     out_uv_offset_width= Vec4f0[]
     for uv in uv_offset_width
-	    push!(out_uv_offset_width, Vec4f0(uv[1], uv[2], uv[3] - uv[1], uv[4] - uv[2]))
+	    push!(out_uv_offset_width, Vec4f0(uv[1], uv[2], uv[3], uv[4] ))
     end
     out_pos_scale = Vec4f0[]
     for (p, sc) in zip(positions2d .+ (aoffset,), scale)
@@ -65,32 +64,42 @@ Overseer.requested_components(::TextRenderer) =
 		TextProgram, IOTarget, FontStorage)
 
 function Overseer.update(::TextRenderer, m::AbstractLedger)
-	spat      = m[Spatial]
-	col       = m[UniformColor]
-	prog      = m[TextProgram][1]
-	cam       = m[Camera3D]
-	vao       = m[TextVao]
-	iofbo     = m[IOTarget][1]
-	persp_mat = cam[1].projview
-	text      = m[Text]
+	spat           = m[Spatial]
+	prog           = singleton(m, TextProgram)
+	camera         = singleton(m, Camera3D)
+	vao            = m[TextVao]
+	iofbo          = singleton(m, IOTarget)
+	persp_mat      = camera.projview
+	projection_mat = camera.proj
+	text           = m[Text]
 	wh = size(iofbo)
 
-	glEnable(GL_DEPTH_TEST)
-	glDepthFunc(GL_LEQUAL)
+	glDisable(GL_DEPTH_TEST)
+	glDepthFunc(GL_ALWAYS)
+	glDisableCullFace()
 
-	glyph_fbo = m[FontStorage][1].storage_fbo
+	glyph_fbo = singleton(m, FontStorage).storage_fbo
 	bind(color_attachment(glyph_fbo, 1))
     bind(iofbo)
     draw(iofbo)
 
 	bind(prog)
-	set_uniform(prog, :canvas_dims, Vec2f0(wh))
+	set_uniform(prog, :resolution, Vec2f0(wh))
     set_uniform(prog, :projview, persp_mat)
-	set_uniform(prog, :glyph_texture, (0, color_attachment(glyph_fbo, 1)))
-	for e in @entities_in(vao && spat && text && col)
-        e_vao, e_spat, e_text, e_col = vao[e], spat[e], text[e], col[e]
-		set_uniform(prog, :start_pos, e_spat.position + e_text.offset)
-		set_uniform(prog, :color, e_col.color)
+    set_uniform(prog, :projection, projection_mat)
+
+    # Fragment uniforms
+	set_uniform(prog, :distancefield, (0, color_attachment(glyph_fbo, 1)))
+	set_uniform(prog, :shape, 3)
+	#TODO make this changeable
+	set_uniform(prog, :stroke_width, 0f0)
+	set_uniform(prog, :glow_width, 0f0)
+	set_uniform(prog, :billboard, true)
+	set_uniform(prog, :scale_primitive,false)
+	for e in @entities_in(vao && spat && text)
+        e_vao, e_spat, e_text = vao[e], spat[e], text[e]
+		set_uniform(prog, :origin, e_spat.position + e_text.offset)
+		set_uniform(prog,:model, m[ModelMat][e].modelmat)
 		bind(e_vao)
 		draw(e_vao)
 	end
